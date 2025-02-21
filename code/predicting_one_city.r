b4de4462068ecde787794605dc8b25d68de970cf
Leo Bastos <lsbastos@gmail.com>
2025-02-21 16:19:12 -0300

    adding a mun.code variable

--- code/predicting_one_city.r
+++ code/predicting_one_city.r
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(httr)
 library(jsonlite)
+library(INLA)
 
 # 
 source(file = "code/aux_fun.r")
@@ -10,23 +11,26 @@ source(file = "code/aux_fun.r")
 
 # Epiweek 41/2015 starts on 2015-10-11
 
+# mun.code = 3304557 # Rio
+mun.code = 4205407 # Floripa
+# mun.code = 3548906 # Sao Carlos - SP
+
 # # Slow for the whole country and depends on internet connection
 df <- get_cases(start_date = "2015-10-11",
                 end_date = today(),
                 disease = 'dengue',
                 # uf=NULL, # RJ
-                muncode= 4205407,
+                muncode= mun.code,
                 # per_page = 100
                )
 
-library(INLA)
 
 
 # Prepare data
 
 df.floripa <- df |> 
   mutate(data_iniSE = ymd(data_iniSE)) |> 
-  prepare.data(suspected_cases = T, muncode = 4205407) 
+  prepare.data(suspected_cases = T, muncode = mun.code) 
 
 # Excluindo a temporada 2024-2025
 df.floripa.inla <- df.floripa |> 
@@ -57,13 +61,26 @@ ggplot( mapping = aes(x = week) ) +
             data = teste$pred,
             linetype = "dashed"
   ) + 
+  geom_pointrange(mapping = aes(y = casos_est, 
+                                ymin = casos_est_min, 
+                                ymax = casos_est_max, 
+                                color = "nowcasting"), 
+             data =    df |> 
+               mutate(
+                 data_iniSE = ymd(data_iniSE),
+                 week = week.season(data_iniSE),
+                 year = season(data_iniSE)) |> 
+               filter(year == "2024-2025") 
+  ) + 
   geom_point(mapping = aes(y = cases, color = "Observed"), 
-            data =  df.floripa |>  
-              filter(year == "2024-2025") 
+             data =  df.floripa |>  
+               filter(year == "2024-2025") 
   ) + 
   labs(
-    colour = "Predictive percentile",
-    y = "Cases"
+    colour = "Predicted cases",
+    y = "Cases",
+    x = "Week ( week 1 = epiweek 41)",
+    title = "Season 2024-2025"
   ) + 
   theme_bw()
 
